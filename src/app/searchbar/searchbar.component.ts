7273e33589ae7858eef26948e9f9cc0a2f8d23e3
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-18 14:07:20 +0300

    added objects

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchbarComponent implements OnInit {
 
+  inputsearch = ''
+  outputresult = []
+  users: any = []
+  reposearch = ''
+  outputRepoSearch = []
+  repos: any = []
+
   constructor() { }
 
   ngOnInit(): void {
