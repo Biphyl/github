c3069329555998c29bebde96ae4f19509da1aec3
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-18 13:55:09 +0300

    generated searchbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DisplayResultComponent } from './display-result/display-result.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    DisplayResultComponent
+    DisplayResultComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
