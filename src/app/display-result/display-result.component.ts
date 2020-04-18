9b019b6b608bcdd7451fbf43ee9e1bb9b9951a15
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-18 14:25:05 +0300

    imported RequestService and passed argument to the contstructor and added app-searchbar tag

--- src/app/display-result/display-result.component.ts
+++ src/app/display-result/display-result.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../user-request/request.service';
 
 @Component({
   selector: 'app-display-result',
   templateUrl: './display-result.component.html',
+  providers:[RequestService],
   styleUrls: ['./display-result.component.css']
 })
 export class DisplayResultComponent implements OnInit {
 
-  constructor() { }
+  constructor(private requestsearvice:RequestService) { }
 
   ngOnInit(): void {
   }
