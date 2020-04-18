ec6f9ae62f0fa2a7bcb9da920a9f27fbb0d65cae
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-18 14:17:21 +0300

    added apiKey,imorted environment and added reposLink and userInfo methods

--- src/app/user-request/request.service.ts
+++ src/app/user-request/request.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RequestService {
+  apikey = environment.apiKey;
+  username: string;
+  private repo: string;
+  constructor(private http: HttpClient) {
 
-  constructor() { }
+  }
+
+
+  reposLink(reposearch) {
+
+
+    return this.http.get<JSON[]>(`https://api.github.com/repos/` + reposearch)
+
+
+
+
+  }
+
+
+
+
+
+
+
+  userInfo(inputsearch) {
+
+    return this.http.get<JSON[]>(`https://api.github.com/users/` + inputsearch)
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "01a41f667cabfaa00b032c3ad9540d5485aac560"
 };
 
 /*
