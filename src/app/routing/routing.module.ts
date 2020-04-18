7e01896bfcd213d4f2da9e274aae227453da7eb8
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-18 14:45:14 +0300

    added custom router

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,12 +1,33 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule,Routes} from '@angular/router'
 
+import { SearchbarComponent } from '../searchbar/searchbar.component';
 
+import { DisplayResultComponent } from '../display-result/display-result.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes:Routes=[
+  {path:"resultdisplay",component:DisplayResultComponent},
+  {path:"searchbar",component:SearchbarComponent},
+  {path:'searchbar/:id',component:SearchbarComponent},
+  {path:"",redirectTo:"/searchbar",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+   
+  ],
+  exports:[RouterModule],
+  declarations: [
+  //   SearchbarComponent,
+  //   //UserdisplayComponent,
+  //   ResultdisplayComponent,
+  //   NotFoundComponent,
   ]
+  
 })
 export class RoutingModule { }
